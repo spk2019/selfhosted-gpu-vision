62531dbb57b01a8956aaa545ac487d28cce76d8f
spk2019 <46295017+spk2019@users.noreply.github.com>
2023-04-16 14:38:50 +0530

    Update train.py

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import matplotlib.pyplot as plt
-
+###
 print("cuda version",torch.version.cuda)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"DEVICE: {torch.cuda.get_device_name()}")
